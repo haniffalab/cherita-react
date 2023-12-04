0ac0db8e78caf8d89a9af7529e2b11fec680f431
dannda <dann.3004@gmail.com>
2023-12-04 15:21:58 +0000

    add PersistentOffCanvas function component that doesnt unmount
    without unmounting offcanvas loses transitions

--- sites/demo/src/components/Offcanvas.js
+++ sites/demo/src/components/Offcanvas.js
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 
 import Offcanvas from "react-bootstrap/Offcanvas";
 
@@ -8,59 +8,81 @@ import {
   SELECTION_MODES,
 } from "@haniffalab/cherita-react";
 
+function PersistentOffcanvas({ show, onHide, scroll, children }) {
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (show && !initialized) {
+      setInitialized(true);
+    }
+  }, [show, initialized]);
+
+  return (
+    <Offcanvas
+      className={show ? "" : "d-none"}
+      backdropClassName={show ? "" : "d-none"}
+      show={initialized}
+      onHide={onHide}
+      scroll={scroll}
+    >
+      {children}
+    </Offcanvas>
+  );
+}
+
 export function OffcanvasObs({ show, handleClose }) {
   return (
-    <Offcanvas show={show} onHide={handleClose}>
+    <PersistentOffcanvas show={show} onHide={handleClose} scroll={true}>
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Categories</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
         <ObsColsList />
       </Offcanvas.Body>
-    </Offcanvas>
+    </PersistentOffcanvas>
   );
 }
 
 export function OffcanvasObsm({ show, handleClose }) {
   return (
-    <Offcanvas show={show} onHide={handleClose}>
+    <PersistentOffcanvas show={show} onHide={handleClose} scroll={true}>
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Embedding space</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>UMAP</Offcanvas.Body>
-    </Offcanvas>
+    </PersistentOffcanvas>
   );
 }
 
 export function OffcanvasVars({ show, handleClose }) {
   return (
-    <Offcanvas show={show} onHide={handleClose}>
+    <PersistentOffcanvas show={show} onHide={handleClose} scroll={true}>
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Features</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
         <VarNamesList mode={SELECTION_MODES.MULTIPLE} />
       </Offcanvas.Body>
-    </Offcanvas>
+    </PersistentOffcanvas>
   );
 }
 
 export function OffcanvasControls({ show, handleClose, Controls }) {
   return (
-    <Offcanvas show={show} onHide={handleClose}>
+    <PersistentOffcanvas show={show} onHide={handleClose} scroll={true}>
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Controls</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
         <Controls />
       </Offcanvas.Body>
-    </Offcanvas>
+    </PersistentOffcanvas>
   );
 }
 
 export function OffcanvasInfo({ show, handleClose }) {
   return (
-    <Offcanvas show={show} onHide={handleClose}>
+    <PersistentOffcanvas show={show} onHide={handleClose} scroll={true}>
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Info</Offcanvas.Title>
       </Offcanvas.Header>
@@ -68,6 +90,6 @@ export function OffcanvasInfo({ show, handleClose }) {
         Some text as placeholder. In real life you can have the elements you
         have chosen. Like, text, images, lists, etc.
       </Offcanvas.Body>
-    </Offcanvas>
+    </PersistentOffcanvas>
   );
 }
