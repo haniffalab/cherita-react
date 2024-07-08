bda2d6ac9edebc1c5b9d6177792a3416593a370d
dannda <dann.3004@gmail.com>
2024-07-08 12:52:49 +0100

    fix scatterplot bounds

--- src/lib/helpers/map-helper.js
+++ src/lib/helpers/map-helper.js
@@ -11,16 +11,15 @@ export class MapHelper {
       longitude: 0,
       latitude: 0,
       zoom: 12,
-      pitch: 0,
-      bearing: 0,
     });
-    let latMin = 50;
-    let latMax = -50;
-    let lonMin = 50;
-    let lonMax = -50;
+    let latMin = Infinity;
+    let latMax = -Infinity;
+    let lonMin = Infinity;
+    let lonMax = -Infinity;
+
+    const RECT_LON_INDEX = 0;
+    const RECT_LAT_INDEX = 1;
     coords.forEach(function (coord) {
-      const RECT_LAT_INDEX = "0";
-      const RECT_LON_INDEX = "1";
       if (coord[RECT_LAT_INDEX] < latMin) latMin = coord[RECT_LAT_INDEX];
       if (coord[RECT_LAT_INDEX] > latMax) latMax = coord[RECT_LAT_INDEX];
       if (coord[RECT_LON_INDEX] < lonMin) lonMin = coord[RECT_LON_INDEX];
@@ -28,11 +27,11 @@ export class MapHelper {
     });
 
     const bounds = [
-      [lonMin, latMax],
-      [lonMax, latMin],
+      [lonMin, latMin],
+      [lonMax, latMax],
     ];
     const { longitude, latitude, zoom } = view.fitBounds(bounds, {
-      padding: { top: 50, bottom: 50, left: 50, right: 50 },
+      padding: { top: 50, bottom: 70, left: 50, right: 50 },
     });
 
     return { longitude, latitude, zoom };
