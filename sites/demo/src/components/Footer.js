eb520ea610174ac7a4b56a823a523aa51fb0dc39
Dave Horsfall <dave@horsfall.dev>
2025-03-28 11:33:46 +0000

    cleanup footer

--- sites/demo/src/components/Footer.js
+++ sites/demo/src/components/Footer.js
@@ -2,12 +2,12 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 export default function Footer() {
   return (
-    <header>
-      <Navbar bg="primary" data-bs-theme="dark">
+    <footer>
+      <Navbar bg="secondary" data-bs-theme="dark">
         <Container fluid>
           <Navbar.Brand href="#">Footer</Navbar.Brand>
         </Container>
       </Navbar>
-    </header>
+    </footer>
   );
 }
