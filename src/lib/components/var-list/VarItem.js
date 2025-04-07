c6c2924f96777a8306e3563c6b877b2e626e17e5
dannda <dann.3004@gmail.com>
2025-04-07 17:26:20 +0100

    use upperfirst instead of capitalize

--- src/lib/components/var-list/VarItem.js
+++ src/lib/components/var-list/VarItem.js
@@ -82,7 +82,7 @@ function VarDiseaseInfoItem(item) {
               if (value !== null && value !== undefined) {
                 return (
                   <tr key={key}>
-                    <td>{_.capitalize(key)}</td>
+                    <td>{_.upperFirst(key)}</td>
                     <td>{value}</td>
                   </tr>
                 );
