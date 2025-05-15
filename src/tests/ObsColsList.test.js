faa7f1a19f127d17c0b2a60765cbc6102298cd6c
dannda <dann.3004@gmail.com>
2025-05-15 16:21:22 +0100

    mock context and hooks in tests

--- src/tests/ObsColsList.test.js
+++ src/tests/ObsColsList.test.js
@@ -5,35 +5,82 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
 import { ObsColsList } from "../lib/components/obs-list/ObsList";
+import { DatasetProvider } from "../lib/context/DatasetContext";
+import { useFetch } from "../lib/utils/requests";
 
 // Mock the context or props
 const mockDataset = {
   varNamesCol: null,
 };
 
-const mockSettings = {
-  selectedObs: { name: "obsName" },
-};
-
-const mockDispatch = jest.fn();
-
-jest.mock("../lib/context/DatasetContext", () => ({
-  useDataset: () => mockDataset,
+jest.mock("@tanstack/react-query-persist-client", () => ({
+  PersistQueryClientProvider: ({ children }) => <>{children}</>,
 }));
 
-jest.mock("../lib/context/SettingsContext", () => ({
-  useSettings: () => mockSettings,
-  useSettingsDispatch: () => mockDispatch,
-}));
+const Wrapper = ({ children }) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+    logger: {
+      log: console.log,
+      warn: console.warn,
+      error: process.env.NODE_ENV === "test" ? () => {} : console.error,
+    },
+  });
 
-const queryClient = new QueryClient();
-
-test("renders ObsColsList component", () => {
-  render(
+  return (
+    // Provide a query client as PersistQueryClientProvider is mocked
     <QueryClientProvider client={queryClient}>
-      <ObsColsList />
+      <DatasetProvider dataset={mockDataset}>{children}</DatasetProvider>
     </QueryClientProvider>
   );
+};
+
+jest.mock("../lib/utils/requests", () => ({
+  useFetch: jest.fn(),
+}));
+
+test("renders ObsColsList component", () => {
+  useFetch.mockReturnValue({
+    fetchedData: null,
+    isPending: true,
+    serverError: null,
+  });
+
+  render(<ObsColsList />, { wrapper: Wrapper });
   const element = screen.getByRole("progressbar");
+  expect(element).toBeInTheDocument();
+});
+
+test("fetches data and renders ObsColsList", async () => {
+  const mockData = [
+    {
+      codes: {
+        False: 0,
+        True: 1,
+      },
+      n_values: 2,
+      name: "boolean",
+      type: "boolean",
+      value_counts: {
+        False: 10,
+        True: 5,
+      },
+      values: ["False", "True"],
+    },
+  ];
+
+  useFetch.mockReturnValue({
+    fetchedData: mockData,
+    isPending: false,
+    serverError: null,
+  });
+
+  render(<ObsColsList />, { wrapper: Wrapper });
+
+  const element = await screen.findByText("boolean");
   expect(element).toBeInTheDocument();
 });
