3fa47ca061d1142e0a440e0407711c5796ee1389
dannda <dann.3004@gmail.com>
2025-05-15 16:54:36 +0100

    add setupTests file

--- src/tests/ObsColsList.test.js
+++ src/tests/ObsColsList.test.js
@@ -1,47 +1,11 @@
 import React from "react";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
+import { Wrapper } from "./setupTests";
 import { ObsColsList } from "../lib/components/obs-list/ObsList";
-import { DatasetProvider } from "../lib/context/DatasetContext";
 import { useFetch } from "../lib/utils/requests";
-
-// Mock the context or props
-const mockDataset = {
-  varNamesCol: null,
-};
-
-jest.mock("@tanstack/react-query-persist-client", () => ({
-  PersistQueryClientProvider: ({ children }) => <>{children}</>,
-}));
-
-const Wrapper = ({ children }) => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-      },
-    },
-    logger: {
-      log: console.log,
-      warn: console.warn,
-      error: process.env.NODE_ENV === "test" ? () => {} : console.error,
-    },
-  });
-
-  return (
-    // Provide a query client as PersistQueryClientProvider is mocked
-    <QueryClientProvider client={queryClient}>
-      <DatasetProvider dataset={mockDataset}>{children}</DatasetProvider>
-    </QueryClientProvider>
-  );
-};
-
-jest.mock("../lib/utils/requests", () => ({
-  useFetch: jest.fn(),
-}));
 
 test("renders ObsColsList component", () => {
   useFetch.mockReturnValue({
