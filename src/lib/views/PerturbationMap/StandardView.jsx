d31f90cc7749b7eaff2f854a0dcfb121ca555811
dannda <dann.3004@gmail.com>
2026-02-10 13:47:23 +0000

    add sidebar hook for obsexplorer

--- src/lib/views/PerturbationMap/StandardView.jsx
+++ src/lib/views/PerturbationMap/StandardView.jsx
@@ -2,41 +2,44 @@ import { useState } from 'react';
 
 import { Card, Container, Modal } from 'react-bootstrap';
 
-import { ObsExplorer } from './ObsExplorer';
-import { ObsColsList } from '../../components/obs-list/ObsList';
 import {
   OffcanvasControls,
-  OffcanvasObs,
   OffcanvasObsm,
-  OffcanvasObsExplorer,
 } from '../../components/offcanvas/OffCanvas';
 import { PseudospatialToolbar } from '../../components/pseudospatial/PseudospatialToolbar';
 import { Scatterplot } from '../../components/scatterplot/Scatterplot';
 import { ScatterplotControls } from '../../components/scatterplot/ScatterplotControls';
-import { SearchBar } from '../../components/search-bar/SearchBar';
 import { SELECTION_MODES } from '../../constants/constants';
 import { DatasetProvider } from '../../context/DatasetContext';
+import { useObsSideBar, useObsExplorerSideBar } from '../../utils/hooks';
 
 export function StandardView({ ...props }) {
-  const [showCategories, setShowCategories] = useState(false);
   const [showEmbeddings, setShowEmbeddings] = useState(false);
-  const [showObsExplorer, setShowObsExplorer] = useState(false);
   const [showControls, setShowControls] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [showPseudospatialControls, setShowPseudospatialControls] =
     useState(false);
+
+  const { ObsSideBar, setShowOffcanvas: setShowCategories } = useObsSideBar({
+    isFullscreen: true,
+    showSelectedAsActive: false,
+    showHistograms: true,
+    showColor: true,
+    ...props,
+  });
+
+  const { ObsExplorerSideBar, setShowOffcanvas: setShowObsExplorer } =
+    useObsExplorerSideBar({
+      isFullscreen: true,
+      mode: SELECTION_MODES.SINGLE,
+    });
 
   return (
     <div className="cherita-app">
       <DatasetProvider {...props}>
         <Container fluid className="cherita-app-container">
           <div className="cherita-app-obs modern-scrollbars border-end h-100">
-            <ObsColsList
-              {...props}
-              showSelectedAsActive={false}
-              showHistograms={true}
-              showColor={true}
-            />
+            <ObsSideBar />
           </div>
           <div className="cherita-app-canvas">
             <Scatterplot
@@ -50,16 +53,7 @@ export function StandardView({ ...props }) {
           <div className="cherita-app-sidebar p-3">
             <Card>
               <Card.Body>
-                <div className="sidebar-features">
-                  <SearchBar
-                    searchDiseases={false}
-                    searchVar={false}
-                    searchObs={true}
-                  />
-                  <div className="sidebar-features-list">
-                    <ObsExplorer />
-                  </div>
-                </div>
+                <ObsExplorerSideBar />
               </Card.Body>
             </Card>
           </div>
@@ -68,17 +62,6 @@ export function StandardView({ ...props }) {
           <Modal.Header closeButton></Modal.Header>
           <Modal.Body></Modal.Body>
         </Modal>
-        <OffcanvasObs
-          show={showCategories}
-          handleClose={() => setShowCategories(false)}
-          {...props}
-          showSelectedAsActive={false}
-        />
-        <OffcanvasObsExplorer
-          show={showObsExplorer}
-          handleClose={() => setShowObsExplorer(false)}
-          mode={SELECTION_MODES.SINGLE}
-        />
         <OffcanvasControls
           show={showControls}
           handleClose={() => setShowControls(false)}
